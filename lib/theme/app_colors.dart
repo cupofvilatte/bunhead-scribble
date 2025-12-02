c8c07f42fa44ee6623a1d932dce8b2f34fa34207
Vilate <viknapp22@gmail.com>
2025-12-02 13:42:38 -0700

    style refactoring: added color theme variables/variable file

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Light Theme
+  static const Color lightPrimary = Colors.pink;
+  static const Color lightAccent = Colors.pink;
+  static const Color lightAppBar = Colors.pink;
+  static const Color lightText = Colors.black87;
+  static const Color lightBackground = Colors.white;
+  static const Color lightBottomNavBackground = Colors.white;
+  static const Color lightBottomNavSelected = Colors.pink;
+  static const Color lightBottomNavUnselected = Colors.grey;
+
+  // Dark Theme
+  static final Color darkBackground = Colors.grey.shade800;
+  static final Color darkAppBar = Colors.grey.shade900;
+  static final Color darkBottomNavBackground = Colors.grey.shade900;
+  static const Color darkPrimary = Colors.pink;
+  static const Color darkAccent = Colors.pinkAccent;
+  static const Color darkText = Colors.white70;
+  static const Color darkBottomNavSelected = Colors.pink;
+  static const Color darkBottomNavUnselected = Colors.grey;
+}
