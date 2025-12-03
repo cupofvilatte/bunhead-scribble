814e1c96f88afc0b0472d25b7c870c35a8817f27
Vilate <viknapp22@gmail.com>
2025-12-03 15:15:42 -0700

    refactored main to use new theme handling functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,85 +1,124 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'theme/app_colors.dart';
+import 'theme/theme_notifier.dart';
 
 import 'pages/home_page.dart';
 import 'pages/calendar_page.dart';
 import 'pages/choreography_page.dart';
 
 void main() {
-  runApp(BunheadScribbleApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => ThemeNotifier(),
+      child: BunheadScribbleApp(),
+    ),
+  );
 }
 
-class BunheadScribbleApp extends StatefulWidget {
+class BunheadScribbleApp extends StatelessWidget {
   const BunheadScribbleApp({super.key});
 
   @override
-  _BunheadScribbleAppState createState() => _BunheadScribbleAppState();
-}
-
-class _BunheadScribbleAppState extends State<BunheadScribbleApp> {
-  bool _isDarkTheme = false;
-
-  void _toggleTheme() {
-    setState(() {
-      _isDarkTheme = !_isDarkTheme;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Bunhead Scribble',
-      theme: ThemeData(
-        brightness: _isDarkTheme ? Brightness.dark : Brightness.light,
+    return Consumer<ThemeNotifier>(
+      builder: (context, themeNotifier, child) {
+        final palette = themeNotifier.currentPalette;
 
-        scaffoldBackgroundColor: _isDarkTheme ? AppColors.darkBackground : AppColors.lightBackground,
-      
-        primaryColor: _isDarkTheme ? AppColors.darkPrimary : AppColors.lightPrimary,
-
-        appBarTheme: AppBarTheme(
-          backgroundColor: _isDarkTheme ? AppColors.darkAppBar : AppColors.lightAppBar,
-
-          foregroundColor: Colors.white,
-        ),
-
-        bottomNavigationBarTheme: BottomNavigationBarThemeData(
-          backgroundColor: _isDarkTheme
-            ? AppColors.darkBottomNavBackground
-            : AppColors.lightBottomNavBackground,
-          selectedItemColor: _isDarkTheme
-            ? AppColors.darkBottomNavSelected
-            : AppColors.lightBottomNavSelected,
-          unselectedItemColor: _isDarkTheme
-            ? AppColors.darkBottomNavUnselected
-            : AppColors.lightBottomNavUnselected,
-        ),
-
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor:
-              _isDarkTheme ? AppColors.darkAccent : AppColors.lightAccent,
-            foregroundColor: Colors.white,
+        return MaterialApp(
+          title: 'Bunhead Scribble',
+          theme: ThemeData(
+            brightness: themeNotifier.isDarkMode ? Brightness.dark : Brightness.light,
+            primaryColor: palette.primary,
+            scaffoldBackgroundColor: palette.background,
+            appBarTheme: AppBarTheme(
+              backgroundColor: palette.appBar,
+              foregroundColor: palette.text,
+            ),
+            bottomNavigationBarTheme: BottomNavigationBarThemeData(
+              backgroundColor: palette.bottomNavBackground,
+              selectedItemColor: palette.bottomNavSelected,
+              unselectedItemColor: palette.bottomNavUnselected,
+            ),
+            elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: palette.accent,
+                foregroundColor: Colors.white,
+              ),
+            ),
+            textTheme: TextTheme(
+              bodyMedium: TextStyle(color: palette.text),
+            ),
           ),
-        ),
-
-        textTheme: TextTheme(
-          bodyMedium: TextStyle(
-            color: _isDarkTheme ? AppColors.darkText : AppColors.lightText,
-          ),
-        ),
-      ),
-      home: MainNavigation(
-        toggleTheme: _toggleTheme, // pass the function down
-      ),
+          home: const MainNavigation(),
+        );
+      },
     );
   }
 }
 
+// class _BunheadScribbleAppState extends State<BunheadScribbleApp> {
+//   bool _isDarkTheme = false;
+
+//   void _toggleTheme() {
+//     setState(() {
+//       _isDarkTheme = !_isDarkTheme;
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Bunhead Scribble',
+//       theme: ThemeData(
+//         brightness: _isDarkTheme ? Brightness.dark : Brightness.light,
+
+//         scaffoldBackgroundColor: _isDarkTheme ? AppColors.darkBackground : AppColors.lightBackground,
+      
+//         primaryColor: _isDarkTheme ? AppColors.darkPrimary : AppColors.lightPrimary,
+
+//         appBarTheme: AppBarTheme(
+//           backgroundColor: _isDarkTheme ? AppColors.darkAppBar : AppColors.lightAppBar,
+
+//           foregroundColor: Colors.white,
+//         ),
+
+//         bottomNavigationBarTheme: BottomNavigationBarThemeData(
+//           backgroundColor: _isDarkTheme
+//             ? AppColors.darkBottomNavBackground
+//             : AppColors.lightBottomNavBackground,
+//           selectedItemColor: _isDarkTheme
+//             ? AppColors.darkBottomNavSelected
+//             : AppColors.lightBottomNavSelected,
+//           unselectedItemColor: _isDarkTheme
+//             ? AppColors.darkBottomNavUnselected
+//             : AppColors.lightBottomNavUnselected,
+//         ),
+
+//         elevatedButtonTheme: ElevatedButtonThemeData(
+//           style: ElevatedButton.styleFrom(
+//             backgroundColor:
+//               _isDarkTheme ? AppColors.darkAccent : AppColors.lightAccent,
+//             foregroundColor: Colors.white,
+//           ),
+//         ),
+
+//         textTheme: TextTheme(
+//           bodyMedium: TextStyle(
+//             color: _isDarkTheme ? AppColors.darkText : AppColors.lightText,
+//           ),
+//         ),
+//       ),
+//       home: MainNavigation(
+//         toggleTheme: _toggleTheme, // pass the function down
+//       ),
+//     );
+//   }
+// }
+
 class MainNavigation extends StatefulWidget {
-  final VoidCallback toggleTheme;
-  
-  const MainNavigation({super.key, required this.toggleTheme});
+  const MainNavigation({super.key});
 
   @override
   _MainNavigationState createState() => _MainNavigationState();
@@ -88,28 +127,43 @@ class MainNavigation extends StatefulWidget {
 class _MainNavigationState extends State<MainNavigation> {
   int _selectedIndex = 0;
 
-  final List<Widget> _pages = [
-    HomePage(),
-    CalendarPage(),
-    ChoreographyPage(),
-  ];
-
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
     });
   }
 
+  final List<Widget> _pages = [
+    HomePage(),
+    CalendarPage(),
+    ChoreographyPage(),
+  ];
+
   @override
   Widget build(BuildContext context) {
+    // Get the notifier directly
+    final themeNotifier = Provider.of<ThemeNotifier>(context);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Bunhead Scribble'),
+        title: const Text('Bunhead Scribble'),
         actions: [
           IconButton(
-            onPressed: widget.toggleTheme,
-            icon: const Icon(Icons.color_lens),
+            onPressed: () => themeNotifier.toggleDarkMode(),
+            icon: Icon(
+              themeNotifier.isDarkMode ? Icons.dark_mode : Icons.light_mode,
+            ),
           ),
+          PopupMenuButton<AppTheme>(
+            icon: const Icon(Icons.palette),
+            onSelected: (theme) => themeNotifier.setTheme(theme),
+            itemBuilder: (context) => AppColors.allThemes
+              .map((theme) => PopupMenuItem(
+                value: theme,
+                child: Text(theme.name),
+                ))
+              .toList(),
+            ),
         ],
       ),
       body: _pages[_selectedIndex],
